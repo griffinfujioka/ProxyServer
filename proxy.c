b0a71ea687bb1434ff7d4defee26c7134b4d7e6d
griffinfujioka <fujiokag@hotmail.com>
2013-11-19 10:57:59 -0800

    Added proxy.c

---
+++ proxy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+static int DEBUG = 1; 
+
+
+int main()
+{
+	printf("================================================="); 
+	printf("\nWelcome to the CptS 455 Proxy Server!"); 
+	printf("\n\tCreated by: Griffin Fujioka"); 
+	printf("\n\tNovember, 2013"); 
+	printf("\n================================================="); 
+
+
+	printf("\nPress any key to exit..."); 
+	char ch = getchar(); 
+	return 0; 
+}
